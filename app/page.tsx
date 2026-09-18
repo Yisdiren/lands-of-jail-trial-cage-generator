35591dd178bf85f0dad8a0718b5930c14a2fd16d
Yisdiren <118637738+Yisdiren@users.noreply.github.com>
2026-09-17 21:59:28 -0400

    Update app/page.tsx to beta 0.40

--- app/page.tsx
+++ app/page.tsx
@@ -733,7 +733,7 @@ export default function Home() {
             or robot reuse
           </p>
         </div>
-        <div className="badge">BETA v0.39</div>
+        <div className="badge">BETA v0.40</div>
       </header>
 
       <section className="panel profile-panel">
