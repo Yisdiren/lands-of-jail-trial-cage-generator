d46863d058a6d0454e38e3ba7aeb8f9b578bbb4b
Yisdiren <118637738+Yisdiren@users.noreply.github.com>
2026-09-18 20:14:04 -0400

    Display Beta v0.48

--- app/page.tsx
+++ app/page.tsx
@@ -512,7 +512,7 @@ export default function Home() {
             or robot reuse
           </p>
         </div>
-        <div className="badge">BETA v0.47</div>
+        <div className="badge">BETA v0.48</div>
       </header>
 
       <section className="panel profile-panel">
